33028b0216e556df8165ea316ad2021eef646533
Daniel Martínez <dmartinez@esliceu.com>
2024-01-31 19:26:24 +0100

    proves Javascript

--- js/provesJS.js
+++ js/provesJS.js
@@ -1,6 +1,8 @@
 _get("#btnCalculaChar").addEventListener("click", function(){calculaChar("#texte", "#resultadoCalculaChar");});
 _get("#btnCalculaChar2").addEventListener("click", function(){calculaChar("#texte2", "#resultadoCalculaChar2");});
 _get("#suma").addEventListener("click", suma);
+_get("#btnMostraPasswd").addEventListener("click", () => mostraPassword("#contrasenya", "#btnMostraPasswd"));
+_get("#btnCanviMode").onclick = canviMode;
 
 function _get(elemento){
     return document.querySelector(elemento);
@@ -17,4 +19,43 @@ function suma(){
     var segon = parseInt(_get("#segon").value);
 
     _get("#resultadoSuma").value = primer + segon;
+}
+
+
+function mostraPassword(inputID, btnID){
+    var contrasenya = _get(inputID);
+    var btnMostraPasswd = _get(btnID);
+    var valorAtrib = contrasenya.getAttribute("type");
+    if (valorAtrib=="text"){
+        contrasenya.setAttribute("type", "password");
+        btnMostraPasswd.value="👁️";
+        btnMostraPasswd.classList.add("vermell");
+        btnMostraPasswd.classList.remove("verd"); 
+    } else if (valorAtrib=="password"){
+        contrasenya.setAttribute("type", "text");
+        btnMostraPasswd.value="⚔️";
+        btnMostraPasswd.classList.add("verd");
+        btnMostraPasswd.classList.remove("vermell"); 
+    }
+}
+
+function canviMode(){
+
+    const body = _get("body");
+    const icona = _get("#btnCanviMode i");
+    if(body.classList.contains("modoDia")) {
+        body.classList.remove("modoDia");
+        body.classList.add("modoNoche");
+        icona.classList.remove("fa-sun");
+        icona.classList.add("fa-moon");
+    } else if(body.classList.contains("modoNoche")) {
+        body.classList.remove("modoNoche");
+        body.classList.add("modoDia");
+        icona.classList.remove("fa-moon");
+        icona.classList.add("fa-sun");
+    }
+}
+
+function afegeixEventInputs (){
+    
 }
