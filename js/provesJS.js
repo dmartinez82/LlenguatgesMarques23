8d3ce0514c899ecb803826d03b312ae3d44366b9
Daniel Martínez <dmartinez@esliceu.com>
2024-02-06 19:23:15 +0100

    proves javascript (innerHTML + objectes + Arrays)

--- js/provesJS.js
+++ js/provesJS.js
@@ -1,10 +1,18 @@
-_get("#btnCalculaChar").addEventListener("click", function(){calculaChar("#texte", "#resultadoCalculaChar");});
-_get("#btnCalculaChar2").addEventListener("click", function(){calculaChar("#texte2", "#resultadoCalculaChar2");});
+_get("#btnCalculaChar").addEventListener("click", function () { calculaChar("#texte", "#resultadoCalculaChar"); });
+_get("#btnCalculaChar2").addEventListener("click", function () { calculaChar("#texte2", "#resultadoCalculaChar2"); });
 _get("#suma").addEventListener("click", suma);
 _get("#btnMostraPasswd").addEventListener("click", () => mostraPassword("#contrasenya", "#btnMostraPasswd"));
 _get("#btnCanviMode").onclick = canviMode;
+_get("#btnAfegeixNom").onclick = afegeixNom;
+_get("#btnMostraResultatNom").onclick = mostraResNom;
 
-function _get(elemento){
+const inputNom = _get("#nom");
+const inputEdad = _get("#edad");
+const divResulNom = _get("#resulNom");
+
+var elementos = [];
+
+function _get(elemento) {
     return document.querySelector(elemento);
 }
 
@@ -14,7 +22,7 @@ function calculaChar(inputID, outputID) {
     _get(outputID).value = texte.length;
 }
 
-function suma(){
+function suma() {
     var primer = parseInt(_get("#primer").value);
     var segon = parseInt(_get("#segon").value);
 
@@ -22,40 +30,78 @@ function suma(){
 }
 
 
-function mostraPassword(inputID, btnID){
+function mostraPassword(inputID, btnID) {
     var contrasenya = _get(inputID);
     var btnMostraPasswd = _get(btnID);
     var valorAtrib = contrasenya.getAttribute("type");
-    if (valorAtrib=="text"){
+    if (valorAtrib == "text") {
         contrasenya.setAttribute("type", "password");
-        btnMostraPasswd.value="👁️";
+        btnMostraPasswd.value = "👁️";
         btnMostraPasswd.classList.add("vermell");
-        btnMostraPasswd.classList.remove("verd"); 
-    } else if (valorAtrib=="password"){
+        btnMostraPasswd.classList.remove("verd");
+    } else if (valorAtrib == "password") {
         contrasenya.setAttribute("type", "text");
-        btnMostraPasswd.value="⚔️";
+        btnMostraPasswd.value = "⚔️";
         btnMostraPasswd.classList.add("verd");
-        btnMostraPasswd.classList.remove("vermell"); 
+        btnMostraPasswd.classList.remove("vermell");
     }
 }
 
-function canviMode(){
+function canviMode() {
 
     const body = _get("body");
     const icona = _get("#btnCanviMode i");
-    if(body.classList.contains("modoDia")) {
+    const spanModo = _get("#titol span");
+    if (body.classList.contains("modoDia")) {
         body.classList.remove("modoDia");
         body.classList.add("modoNoche");
         icona.classList.remove("fa-sun");
         icona.classList.add("fa-moon");
-    } else if(body.classList.contains("modoNoche")) {
+        spanModo.textContent = "(modo noche)";
+    } else if (body.classList.contains("modoNoche")) {
         body.classList.remove("modoNoche");
         body.classList.add("modoDia");
         icona.classList.remove("fa-moon");
         icona.classList.add("fa-sun");
+        spanModo.textContent = "(modo dia)";
     }
 }
 
-function afegeixEventInputs (){
-    
+let indice = 0;
+
+function afegeixNom() {
+    let informacion = {
+        id: indice++,
+        nombre: inputNom.value,
+        edad: inputEdad.value
+    };
+
+    var trobat = false;
+    elementos.forEach(function(elemento){
+        if (elemento.nombre ==   informacion.nombre) trobat = true;
+    });
+
+    if (informacion.nombre.length != 0 && informacion.edad.length != 0 && !trobat){
+        elementos.push(informacion);
+    }
+        
+    inputNom.value = ""; //limpiamos el input
+    inputEdad.value = ""; //limpiamos el input
+
+    mostraResNom();
+}
+
+function mostraResNom() {
+    let resultado = "";
+
+    elementos.forEach(function(elemento, indice){
+
+    });
+
+
+    for (let elemento of elementos) {
+        resultado += `<tr><td>${elemento.id}</td><td>${elemento.nombre}</td><td>${elemento.edad}</td></tr>`;
+    }
+
+    divResulNom.querySelector("tbody").innerHTML = resultado;
 }
