6dec6bb4f61e805a2023d6c869b918b18fd22695
Daniel Martínez <dmartinez@esliceu.com>
2024-02-27 19:08:48 +0100

    continuamos con localStorage

--- js/localStorage.js
+++ js/localStorage.js
@@ -1,39 +1,95 @@
 const nom = document.getElementById("nom");
 const edad = document.getElementById("edad");
+const username = document.getElementById("username");
+const limpiar = document.getElementById("limpiar");
 
 const form = document.querySelector("form");
 const selDades = document.querySelector("#selDades");
 
-form.onsubmit = function(event){
+form.onsubmit = function (event) {
     event.preventDefault();
 
-    let datosRecuperados = sessionStorage.getItem("datos");
-    let objetosRecuperados = [];
-    if (datosRecuperados != null){
-        objetosRecuperados = JSON.parse(datosRecuperados);
+    let objetosRecuperados = obteArraySessionStorage("datos");
+
+    const nuevoObjeto = creaObjeto();
+
+    let encontrado = false;
+    objetosRecuperados.forEach(element => {
+        if (element.username == nuevoObjeto.username) {
+            encontrado = true;
+            return;
+        }
+    });
+
+    if (encontrado) {
+        alert("Este usuario ya existe!");
+    } else {
+        objetosRecuperados.push(nuevoObjeto);
+        sessionStorage.setItem("datos", JSON.stringify(objetosRecuperados));
+
+        actualizaSelect(objetosRecuperados);
+
+        this.reset();
     }
 
-    objetosRecuperados.push(creaObjeto());
-    sessionStorage.setItem("datos", JSON.stringify(objetosRecuperados));
-
-    actualizaSelect(objetosRecuperados);
 }
 
-function actualizaSelect(objetos){
+function actualizaSelect(objetos) {
     selDades.innerHTML = "";
 
     let resultado = "";
     for (const objeto of objetos) {
-        resultado += `<option>${objeto.nombre} (Edad: ${objeto.edad})</option>`; 
+        resultado += `<option value="${objeto.username}">${objeto.nombre} (Edad: ${objeto.edad})</option>`;
     }
 
     selDades.innerHTML = resultado;
-
 }
 
-function creaObjeto(){
+function creaObjeto() {
     return {
         nombre: nom.value,
-        edad: edad.value
+        edad: edad.value,
+        username: username.value
     }
+}
+
+selDades.onchange = function () {
+
+    const datosElegidos = selDades.value;
+
+    let objetosRecuperados = obteArraySessionStorage("datos");
+
+    let objetoEncontrado = null;
+    for (const objeto of objetosRecuperados) {
+        if (objeto.username == datosElegidos) {
+            objetoEncontrado = objeto;
+            break;
+        }
+    }
+
+    if (objetoEncontrado) {
+        nom.value = objetoEncontrado.nombre;
+        edad.value = objetoEncontrado.edad;
+        username.value = objetoEncontrado.username;
+    }
+}
+
+limpiar.onclick = () => {
+    sessionStorage.clear();
+    actualizaSelect([]);
+}
+
+document.body.onload = () => {
+    actualizaSelect(obteArraySessionStorage("datos"));
+}
+
+function obteArraySessionStorage(param) {
+    let objetosRecuperados = [];
+    let datosRecuperados = sessionStorage.getItem(param);
+
+    if (datosRecuperados != null) {
+        objetosRecuperados = JSON.parse(datosRecuperados);
+    }
+
+    return objetosRecuperados;
 }
